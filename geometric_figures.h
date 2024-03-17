299d6d152b9044c607750221d2d183b6580a5455
madeInHeavenAct4 <dmitry.svitkov1@gmail.com>
2024-03-17 20:46:07 +0700

    треугольник

--- geometric_figures.h
+++ geometric_figures.h
@@ -5,3 +5,11 @@ double rectanglePerimeter(double sideOne, double sideTwo);
 double rectangleArea(double sideOne, double sideTwo);
 
 double rectangleDiagonal(double sideOne, double sideTwo);
+
+
+
+int triangleP(int sideA, int sideB, int sideC)
+
+void isTriangle(int sideA, int sideB, int sideC)
+
+float triangleS(int sideA, int sideB, int sideC, int P)
---
+++ triangle.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int triangleP(int sideA, int sideB, int sideC)
+{
+    int P;
+    if (((sideA + sideB) < sideC) or ((sideA + sideC) < sideB) or ((sideB + sideC) < sideA))
+        cout << "Треугольник не существует" << endl;
+    else
+        P = sideA + sideB + sideC;
+    return P;
+}
+
+void isTriangle(int sideA, int sideB, int sideC)
+{
+    if (((sideA + sideB) < sideC) or ((sideA + sideC) < sideB) or ((sideB + sideC) < sideA))
+        cout << "Треугольник не существует" << endl;
+    else
+        if ((sideA == sideB) or (sideA == sideC) or (sideB == sideC))
+            cout << "Равнобедренный" << endl;
+
+}
+
+float triangleS(int sideA, int sideB, int sideC, int P)
+{
+    float S;
+    if (((sideA + sideB) < sideC) or ((sideA + sideC) < sideB) or ((sideB + sideC) < sideA))
+        cout << "Треугольник не существует" << endl;
+    else
+    {
+        float p = P / 2;
+        S = sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+    }
+    return S;
+}
