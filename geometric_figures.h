763285a3552129d6496fa148549cc5b91b6fb893
nemesisxgod <mtoreno1999@gmail.com>
2024-03-17 16:33:06 +0300

    perimeter_area_diagonal_of_rectangle

---
+++ geometric_figures.h
@@ -0,0 +1,7 @@
+#include <cmath>
+
+double rectanglePerimeter(double sideOne, double sideTwo);
+
+double rectangleArea(double sideOne, double sideTwo);
+
+double rectangleDiagonal(double sideOne, double sideTwo);
